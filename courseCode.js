55faf6297db6e1198c942883495e7c9cd278869d
K Sai Kiran Naik <naik11665@gmail.com>
2024-12-21 20:28:16 +0530

    minor changes

---
+++ courseCode.js
@@ -0,0 +1,35 @@
+// Copy functionality
+document.querySelectorAll('.copy-field').forEach(function(cell) {
+    cell.addEventListener('click', function() {
+        this.title = "copied!";
+
+        const tempInput = document.createElement('input');
+        tempInput.value = cell.innerText;
+        document.body.appendChild(tempInput);
+
+        tempInput.select();
+        tempInput.setSelectionRange(0, 99999);
+
+        document.execCommand('copy');
+
+        document.body.removeChild(tempInput);
+    });
+});
+
+// Search functionality
+const searchInput = document.getElementById('search');
+const tableRows = document.querySelectorAll('#course-table tr');
+
+searchInput.addEventListener('input', function() {
+    const searchValue = searchInput.value.toLowerCase();
+
+    tableRows.forEach(row => {
+        const course = row.cells[1].innerText.toLowerCase();
+
+        if (course.includes(searchValue)) {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+});
--- webonixs.js
+++ webonixs.js
@@ -1,4 +1,3 @@
-
 let form = document.querySelector('form');
 let input1 = document.querySelector('#rollNo');
 // let select = document.querySelector('#subject');
@@ -10,6 +9,9 @@ let final = document.querySelector("#final_doc");
 let div = document.querySelector(".form-div");
 let div2 = document.querySelector(".pdf-div");
 let act = "";
+
+let chgtheme = document.querySelector('#style-chg');
+
 form.addEventListener('submit',function(event){
     event.preventDefault();
 
@@ -23,9 +25,23 @@ form.addEventListener('submit',function(event){
 
     act = `https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/${rollno}/LAB/SEM${sem}/${subjectCode}/${rollno}_week${weekno}.pdf`;
     final.src = act;
+    chgtheme.style.display="none";
     div2.style.display = "block";
     div2.scrollIntoView({
         behavior: 'smooth', 
         block: 'start' 
     });
 })
+
+function changeStylesheet() {
+
+    const stylesheet = document.getElementById('theme-stylesheet');
+
+    if (stylesheet.getAttribute('href') === 'style1.css') {
+        chgtheme.title="POOKIE MODE: OFF!";
+        stylesheet.setAttribute('href', 'style2.css');
+    } else {
+        chgtheme.title="POOKIE MODE: ON!"
+        stylesheet.setAttribute('href', 'style1.css');
+    }
+}
